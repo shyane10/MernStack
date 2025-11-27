7df6646bdf3906dd455a6a6c35410b4c06661abe
shyane10 <poudelishan656@gmail.com>
2025-11-27 14:53:21 +0545

    getApi

--- app.js
+++ app.js
@@ -1,65 +1,3 @@
-
-// alternative way 
-const{connectDatabase} = require("./database/database");
-const Blog = require("./Model/BlogModel")
-const express = require("express")
-const app = express()
-connectDatabase();
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-    console.log("Mangodb sanga connect vayo hai!");
-
-app.get("/",(req,res)=>{
-    res.json({
-        message:"yo home page hai",
-    })
-})
-
-app.get("/contact",(haha,huhu)=>{
-    huhu.send("This is Contact Page Hai");
-})
-
-
-app.get("/",(req,res)=>{
-    res.send("Hello I am From Home Page")
-})
-
-app.listen(3000,(req,res)=>{
-    console.log("Node js started at port 3000")
-});
-
-
-//CREATE BLOG API
-
-app.post("/createBlog",async(req,res)=>{
-
-    const title = req.body.title;
-    const subtitle = req.body.subtitle;
-    const description = req.body.description;
-
-//Alternative (object destructuring)
-// const {title,subtitle,description} = req.body
-console.log(req.body)
-
-
-// Insert to database logic goes here
-
-await Blog.create({
-    title,
-    subtitle,
-    description,
-});
-
-
-
-    res.json({
-
-        status: 201,
-        message: "Blog created successfully",
-    });
-});
-
 // mongodb+srv://username:password@123@cluster0.ens7aae.mongodb.net/?appName=Cluster0
 
 // mongodb+srv://username:ishan@cluster0.fnmeotc.mongodb.net/?appName=Cluster0
@@ -90,6 +28,86 @@ await Blog.create({
 // });
 
 
-
-
 // const app = require("express")()
+
+
+
+// alternative way 
+const { connectDatabase } = require("./database/database");
+const Blog = require("./Model/BlogModel")
+const express = require("express")
+
+const app = express();
+
+// Connect Database
+connectDatabase();
+console.log("Mongodb sanga connect vayo hai!");
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ROUTES --------------------------------------
+
+// HOME ROUTE
+app.get("/", (req, res) => {
+    res.send("Hello I am From Home Page");
+});
+
+// CONTACT ROUTE
+app.get("/contact", (req, res) => {
+    res.send("This is Contact Page Hai");
+});
+
+// CREATE BLOG API
+app.post("/createBlog", async (req, res) => {
+    const { title, subtitle, description } = req.body;
+
+    try {
+        await Blog.create({ title, subtitle, description });
+
+        res.json({
+            status: 201,
+            message: "Blog created successfully",
+        });
+    } catch (err) {
+        res.json({
+            status: 500,
+            message: "Error creating blog",
+            error: err.message
+        });
+    }
+});
+
+// GET ALL BLOGS
+app.get("/blogs", async (req, res) => {
+    try {
+        const blogs = await Blog.find();
+
+        if (blogs.length === 0) {
+            return res.json({
+                status: 404,
+                message: "Empty Blog",
+                data: [z]
+            });
+        }
+
+        res.json({
+            status: 200,
+            message: "Blogs fetched successfully",
+            data: blogs
+        });
+
+    } catch (err) {
+        res.json({
+            status: 500,
+            message: "Error fetching blogs",
+            error: err.message
+        });
+    }
+});
+
+// SERVER --------------------------------------
+app.listen(3000, () => {
+    console.log("Node js started at port 3000");
+});
